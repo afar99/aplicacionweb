14600050b7d616094fcc1b9a6e4750ba8c38974a
afar99 <armando.ruiz019@gmail.com>
2023-11-13 17:44:37 -0600

    asd

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def login():
         try:
             with connection.cursor(dictionary=True) as cursor:
                 # Ejecuta tu consulta SQL
-                sql = 'SELECT * FROM usuarios WHERE usuario = %s AND contrasena = %s' 
+                sql = 'SELECT * FROM usuarios ' 
                 cursor.execute(sql)
                 result = cursor.fetchall()
         finally:
