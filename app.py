040b11735721fbc1301a1c8198090fec907a968a
afar99 <armando.ruiz019@gmail.com>
2023-11-13 20:25:56 -0600

    asa

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ user = 'admin'
 password = 'Tesis12345+'
 database = 'coladeras'
 
-
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -22,10 +20,11 @@ def login():
         connection = mysql.connector.connect(
             user=user,
             password=password,
-            host='basetesis.cexq7m1jqe1w.us-east-2.rds.amazonaws.com',
+            host=host,
             database=database,
             port=3306
         )
+
         try:
             with connection.cursor(dictionary=True) as cursor:
                 # Ejecuta tu consulta SQL
@@ -35,17 +34,48 @@ def login():
 
                 if user_data:
                     # Inicio de sesión exitoso, guarda la información del usuario en la sesión
-                   # session['user_id'] = user_data['id']
+                    session['user_id'] = user_data['id']
                     return redirect(url_for('obtener_datos'))
                 else:
                     # Credenciales incorrectas, redirige a la página de inicio de sesión
-                      return render_template('login.html')
+                    return render_template('login.html')
         finally:
             connection.close()
 
-       
-
     return render_template('login.html')
+
+
+@app.route('/registro', methods=['GET', 'POST'])
+def registro():
+    if request.method == 'POST':
+        # Obtén los datos del formulario de registro
+        username = request.form['username']
+        password = request.form['password']
+        nombre = request.form['nombre']
+        correo = request.form['correo']
+
+        # Conéctate a la base de datos
+        connection = mysql.connector.connect(
+            user=user,
+            password=password,
+            host=host,
+            database=database,
+            port=3306
+        )
+
+        try:
+            with connection.cursor(dictionary=True) as cursor:
+                # Inserta los datos en la tabla usuarios
+                sql = 'INSERT INTO usuarios (usuario, contrasena, nombre, correo) VALUES (%s, %s, %s, %s)'
+                cursor.execute(sql, (username, password, nombre, correo))
+                connection.commit()
+
+                # Redirige a la página de inicio de sesión después del registro exitoso
+                return redirect(url_for('login'))
+        finally:
+            connection.close()
+
+    return render_template('registro.html')
 
 
 @app.route('/obtener_datos', methods=['GET'])
@@ -70,6 +100,7 @@ def obtener_datos():
 
     # Devuelve los datos como HTML usando una plantilla
     return render_template('index.html', result=result)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
